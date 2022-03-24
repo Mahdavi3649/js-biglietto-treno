13af068b2f4632539faee04c0644ba3842698687
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-24 15:32:03 +0100

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// il calcolo del prezzo del biglietto del treno
+
+// chiedere all'utente il numero di chilometri che vuole percorrere
+
+const numero_km = parseInt(prompt("Quanti chilometri vuoi percorrere?!"))
+console.log(numero_km);
+
+// l'età del passeggero
+const età_passaggero = parseInt(prompt("Quanti anni hai?!"))
+console.log(età_passaggero);
+
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole

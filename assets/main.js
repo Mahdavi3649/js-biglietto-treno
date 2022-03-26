3941e286093f22d10fb8b16f337b2707eae6062f
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-26 22:36:31 +0100

    Ottimizzato il Codice

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,48 @@
 // il calcolo del prezzo del biglietto del treno
 
 
-// chiedere all'utente il numero di chilometri che vuole percorrere
-const numero_km = parseInt(prompt("Quanti chilometri vuoi percorrere?!"))
-console.log(numero_km);
+//chiedere all'utente il numero di chilometri
+const km = parseInt(prompt("quanti km vuoi percorrere?"))
+if (isNaN(km)){
+    alert("devi inserire un numero per i km da percorrere")
+}
 
+//che vuole percorrere e l'età del passeggero
+const age = parseInt(prompt("quanti anni hai"))
+if (isNaN(age)){
+    alert("devi inserire un numero")
+}
 
-// l'età del passeggero
-const età_passaggero = parseInt(prompt("Quanti anni hai?!"))
-console.log(età_passaggero);
-
-
-/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
- secondo queste regole */
-
+console.log(km, age);
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzo_biglietto = numero_km * 0.21;
-console.log(prezzo_biglietto);
-
+const prezzo_al_km = 0.21;
+let prezzo_finale;
+const prezzo_standard = km * prezzo_al_km;
+//console.log(prezzo_standard);
 
 // sconto del 20% per i minorenni
-if (età_passaggero <= 18){
-    var calcoloSconto_Under = ((prezzo_biglietto * 20) / 100);
-    var biglietto_Under = prezzo_biglietto - calcoloSconto_Under;
-    console.log(biglietto_Under.toFixed(2));
+const prezzo_minnoreni = prezzo_standard - (prezzo_standard * 0.2)
+
+//uno sconto del 40% per gli over 65
+const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
+
+//console.log(prezzo_minnoreni, prezzo_over_65);
 
 
-// sconto del 40% per gli over 65
-} else if (età_passaggero >= 65){
-    var calcoloSconto_Over = ((prezzo_biglietto * 40) / 100);
-    var biglietto_Over = prezzo_biglietto - calcoloSconto_Over;
-    console.log(biglietto_Over.toFixed(2));
+if(age <= 18){
+    messaggio = "hai diritto al prezzo di scontato 20%";
+    prezzo_finale = prezzo_minnoreni
 
-}else{
-    console.log("Nessuno sconto applicabile e Buon viaggio ahahaha");
+}else if(age >= 65){
+    messaggio = "hai diritto al prezzo di scontato 40%";
+    prezzo_finale = prezzo_over_65
+} else{
+    messaggio = "mi dispace, nessun sconto applicabile";
+    prezzo_finale = prezzo_standard
 }
+//L'output del prezzo finale va scritto in forma umana
+
+console.log(`${messaggio}, prezzo bigliett: Euro ${prezzo_finale.toFixed(2)}`);
+
 
